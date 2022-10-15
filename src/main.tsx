ab77ae14d67e980be640f4049999c2bcf83678b7
Vinicius Ribeiro <vinicius.vrf@gmail.com>
2022-10-14 23:55:08 -0300

    ✨ Add feature manifest.json

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import './styles.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { registerSW } from 'virtual:pwa-register';
 
 import { Router } from './routes';
+
+if ('serviceWorker' in navigator) {
+  // && !/localhost/.test(window.location)) {
+  registerSW();
+}
 
 window.addEventListener('DOMContentLoaded', () => {
   const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
